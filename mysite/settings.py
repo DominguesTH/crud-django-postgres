5ba632d98100037f5cce3277a36168ae56a6d24d
Thiago Domingues <107176452+DominguesTH@users.noreply.github.com>
2023-01-19 04:45:25 -0300

    fix in database

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["PGDATABASE"],
-#         'USER': os.environ["PGUSER"],
-#         'PASSWORD': os.environ["PGPASSWORD"],
-#         'HOST': os.environ["PGHOST"],
-#         'PORT': os.environ["PGPORT"],
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': os.environ["PGDATABASE"],
+         'USER': os.environ["PGUSER"],
+         'PASSWORD': os.environ["PGPASSWORD"],
+         'HOST': os.environ["PGHOST"],
+         'PORT': os.environ["PGPORT"],
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
